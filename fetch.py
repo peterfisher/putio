a8633b3b066935a77d96093102b35f11c8ef0036
Peter Fisher <peter@phyn3t.com>
2012-03-17 21:11:44 -0700

    Fixed bugs, optimized checking local files.

--- fetch.py
+++ fetch.py
@@ -5,8 +5,33 @@ import os
 import httplib2
 import ConfigParser
 import sys
+import time
 
 class fetch:
+
+    def get_items(self, parent=None, id_Number=None):
+        """ Handles putio.get_items request with better error handling.
+
+        """
+
+        sigkill = 0
+
+        while 1:
+            try:
+                value = self.API.get_items(parent_id=parent, id=id_Number)
+                return value
+            except putio.PutioError as err:
+                if str(err) == "You have no items to show.":
+                    return []
+                print("Warning: Fail to make putio request. Trying again.")
+                print("Debug: " + str(err))
+                sigkill += 1
+                if sigkill >= 3:
+                    print("ERROR: Request failed too many times. Giving up!")
+                    break
+                time.sleep(15)
+                pass
+
 
     def getAllItems(self):
         """Get entire put.io account and and save it to a dict called contents.
@@ -18,12 +43,12 @@ class fetch:
 
         control = []
 
-        for folder in self.API.get_items():
+        for folder in self.get_items():
             control.append(folder.id)
 
         while control.__len__() > 0:
             try:
-                for item in self.API.get_items(parent_id=control.pop()):
+                for item in self.get_items(parent=control.pop()):
                     print("Getting content for: " + item.name)
                     if item.type == "folder":
                         control.insert(0,item.id)
@@ -45,14 +70,14 @@ class fetch:
         returns a string that contains the path of the file /foo/bar.txt
         """
 
-        path = self.API.get_items(id=itemID)[0].name
+        path = self.get_items(id_Number=itemID)[0].name
 
-        while (True):
-            itemID = self.API.get_items(id=itemID)[0].parent_id
+        while 1:
+            itemID = self.get_items(id_Number=itemID)[0].parent_id
             if itemID == "0":
                 break
             else:
-                path = self.API.get_items(id=itemID)[0].name + "/" + path
+                path = self.get_items(id_Number=itemID)[0].name + "/" + path
         return  self.dlLocation + path
 
     def createLocalDirectory(self, file=None):
@@ -71,31 +96,15 @@ class fetch:
             print("ERROR: Could not create directory for file")
             print(str(err))
 
-    def checkLocalFile(self, file=None):
-        """"Check whether a local file exists.
-
-        file is a path to a local file in format: /foo/bar.txt
-        """
-
-        if file == None:
-            raise ValueError("Pass a file path!")
-
-        try:
-            if os.path.exists(file):
-                return True
-            else:
-                return False
-        except OSError as err:
-            print(
-                "ERROR: Couldn;t read filesystem to determine file existence")
-            print(str(err))
-
     def fetchPutIOFile(self, fileID=None, fileDir=None):
         """We will fetch a file and store it locally.
+
 
         fileID - is the file ID of the file you want to download from put.io
         fileDir - The file name and local directory to save the file.
         """
+
+        sigkill = 0
 
         if fileID == None or fileDir == None:
             raise ValueError('You must send a putio item id and file path.')
@@ -106,17 +115,28 @@ class fetch:
         fileName = self.contents[fileID]['name']
 
         print("Downloading file: " + fileName)
-        resp, data = self.h.request(self.contents[fileID]['uri'])
-        if resp.status >= 400:
-            print("We couldn't downlaod the file: " + fileName)
-            print(resp)
-        else:
-            with open(fileDir, 'wb') as file:
-                file.write(data)
+        while 1:
+            try:
+                resp, data = self.h.request(self.contents[fileID]['uri'])
+                if resp.status >= 400:
+                    print("We couldn't downlaod the file: " + fileName)
+                    print(resp)
+                else:
+                    with open(fileDir, 'wb') as file:
+                        file.write(data)
+                        break
+            except httplib2.HttpLib2Error as err:
+                print("Warning we had a httplib2 error... Trying again.")
+                print(str(err))
+                if sigkill <= 3:
+                    sigkill += 1
+                    pass
+                else:
+                    print("ERROR: We failed to fetch item 3 items. Fuck this.")
 
     def getItem(self, fileID=None):
         try:
-            putData = self.API.get_items(id=fileID)
+            putData = self.get_items(id_Number=fileID)
             for item in putData:
                 self.contents[fileID] = {
                     'name': item.name, 'parentID': item.parent_id,
@@ -126,11 +146,28 @@ class fetch:
             print("Couldn't download file")
             print(str(err))
 
+    def getLocalFiles(self):
+        """Gets all the files in our local putio directory
+
+        The reason for this function is os.path.exists is so slow. Walking
+        the local directory and then doing a list lookup is a lot faster.
+
+        returns a list containing the full path of each file in our local putio
+        directory.
+        """
+
+        for root, dirs, files in os.walk(self.dlLocation):
+            for name in files:
+                self.localStore.append(os.path.join(root, name))
+
+        return self.localStore
 
     def __init__(self,configLocation=None):
 
         #Global shit
         self.contents = {}
+        self.localStore = []
+
         if configLocation == None or not os.path.exists(configLocation):
             raise ValueError("Provide Valid Config FILE!")
             sys.exit(1)
